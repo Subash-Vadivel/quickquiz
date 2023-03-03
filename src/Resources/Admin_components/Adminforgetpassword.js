406511c2860c7b0d020a76e13495027d5725fc34
Subash-Vadivel <88760720+Subash-Vadivel@users.noreply.github.com>
2023-03-03 15:20:05 +0530

    l

--- src/Resources/Admin_components/Adminforgetpassword.js
+++ src/Resources/Admin_components/Adminforgetpassword.js
@@ -1,9 +1,44 @@
 import React from 'react'
 import { useState } from 'react';
 import styleloginform from '../Admin_css/adminloginform.module.css'
-export default function Adminforgetpassword() {
+export default function Adminforgetpassword(props) {
   const [email,setEmail]=useState('');
+  const [opt,setOtp]=useState();
+  const [verification,setVerification]=useState(true);
+  const submit=()=>{
+             setVerification(!verification);
+  }
   return (
+    <>
+    {verification?
+(
+<div className={`Auth-form-container ${styleloginform.formWrapper}`}>
+      <form className="Auth-form">
+        <div className="Auth-form-content ">
+          <h3 className="Auth-form-title">OTP</h3>
+          <div className="form-group mt-3">
+            <label>Enter OTP</label>
+            <input
+            value={opt}
+              type="number"
+               
+             className="form-control mt-1"
+              placeholder="Enter email"
+              onChange={(e)=>{setOtp(e.target.value)}}
+            />
+          </div>
+          <div className="d-grid gap-2 mt-3">
+            <button type="submit" className="btn btn-primary" onClick={submit}>
+              Reset
+            </button>
+          </div>
+          
+        </div>
+      </form>
+    </div>
+    )
+    :
+(
 <div className={`Auth-form-container ${styleloginform.formWrapper}`}>
       <form className="Auth-form">
         <div className="Auth-form-content ">
@@ -20,7 +55,7 @@ export default function Adminforgetpassword() {
             />
           </div>
           <div className="d-grid gap-2 mt-3">
-            <button type="submit" className="btn btn-primary">
+            <button type="submit" className="btn btn-primary" onClick={submit}>
               Reset
             </button>
           </div>
@@ -28,5 +63,8 @@ export default function Adminforgetpassword() {
         </div>
       </form>
     </div>
+)
+    }
+    </>
   )
 }
