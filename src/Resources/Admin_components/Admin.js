0569ade0b982e7e96cae718947a8144253f4927e
Subash-Vadivel <88760720+Subash-Vadivel@users.noreply.github.com>
2023-03-03 11:30:37 +0530

    h

--- src/Resources/Admin_components/Admin.js
+++ src/Resources/Admin_components/Admin.js
@@ -1,13 +1,8 @@
 import React from 'react'
-import { Link, Outlet, useNavigate } from 'react-router-dom'
-import { useAuth } from '../../Authentication'
+import {  Outlet} from 'react-router-dom'
 
 export default function Admin() {
-  const auth=useAuth();
-  if(auth.user)
-  {
-        <Link to='dashboard'></Link>
-  }
+  
   return (
     <>
     <Outlet/>
