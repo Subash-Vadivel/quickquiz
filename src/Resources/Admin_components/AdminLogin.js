9db448deb5cc3fd6788962be78e80761029b1fd9
Subash-Vadivel <88760720+Subash-Vadivel@users.noreply.github.com>
2023-03-03 13:40:36 +0530

    hi

--- src/Resources/Admin_components/AdminLogin.js
+++ src/Resources/Admin_components/AdminLogin.js
@@ -1,30 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container, Row ,Col} from 'react-bootstrap'
 import stylelogin from '../Admin_css/adminlogin.module.css'
 export default function AdminLogin() {
+  const [userid,setUserID]=useState();
+  const [password,setPassword]=useState();
   return (
     <>
-    <Container className={stylelogin.loginWrapper}>
+    <Container fluid className={stylelogin.loginWrapper}>
       <Row className='align-items-center vh-100'>
         <Col md={4} sm={2} xs={1}></Col>
         
-        <Col md={4} sm={2} xs={1}>
+        <Col md={4} sm={8} xs={10}>
 
-        <div className="Auth-form-container">
+        <div className={`Auth-form-container ${stylelogin.formWrapper}`}>
       <form className="Auth-form">
         <div className="Auth-form-content">
           <h3 className="Auth-form-title">Log In</h3>
           <div className="form-group mt-3">
             <label>Email address</label>
             <input
+            value={userid}
               type="email"
               className="form-control mt-1"
               placeholder="Enter email"
+              onChange={(e)=>{setUserID(e.target.value)}}
             />
           </div>
           <div className="form-group mt-3">
             <label>Password</label>
             <input
+            value={password}
               type="password"
               className="form-control mt-1"
               placeholder="Enter password"
