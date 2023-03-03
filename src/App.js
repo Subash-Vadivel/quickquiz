f68dc01e387956e82d340e038dde41f6c48a0fd4
Subash-Vadivel <88760720+Subash-Vadivel@users.noreply.github.com>
2023-03-03 13:53:24 +0530

    dsd

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Authentication from './Authentication';
-import Admin from './Resources/Admin_components/Admin'
+import Admin from './Resources/Admin_Pages/Admin'
 import "bootstrap/dist/css/bootstrap.min.css";
-import AdminLogin from './Resources/Admin_components/AdminLogin';
+import AdminLogin from './Resources/Admin_Pages/AdminLogin';
 import Home from './Resources/User_components/Home';
-import AdminPanel from './Resources/Admin_components/AdminPanel';
-import AdminLoginRequired from './Resources/Admin_components/AdminLoginRequired';
+import AdminPanel from './Resources/Admin_Pages/AdminPanel';
+import AdminLoginRequired from './Resources/Admin_Pages/AdminLoginRequired';
 function App() {
   return (
     <>
