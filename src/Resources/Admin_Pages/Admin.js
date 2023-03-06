1aceefa16ebc5027c846de2f5349c90168624cd4
Subash-Vadivel <88760720+Subash-Vadivel@users.noreply.github.com>
2023-03-06 17:20:43 +0530

    t

--- src/Resources/Admin_Pages/Admin.js
+++ src/Resources/Admin_Pages/Admin.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import {  Outlet} from 'react-router-dom'
 import Sidebar from '../Admin_components/Sidebar'
-
+import styles from '../Admin_css/sidebar.module.css'
 export default function Admin() {
   
   return (
-    <>
+    <div className={styles.dFlex}>
     <Sidebar/>
     <Outlet/>
-    </>
+    </div>
   )
 }
