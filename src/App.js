258035bc76b752808acfe498e2040036870a854d
kameswaran <kameswaran122003@gmail.com>
2023-03-06 21:20:02 +0530

    dashboard

--- src/App.js
+++ src/App.js
@@ -1,42 +1,18 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Authentication from './Authentication';
-import Dashboard from './Resources/Admin_Pages/AdminDashboard';
-import Admin from './Resources/Admin_Pages/Admin'
-import "bootstrap/dist/css/bootstrap.min.css";
-import AdminLogin from './Resources/Admin_Pages/AdminLogin';
-import Home from './Resources/User_components/Home';
-import AdminPanel from './Resources/Admin_Pages/AdminPanel';
-import AdminLoginRequired from './Resources/Admin_Pages/AdminLoginRequired';
+import './App.css'
+import MainDash from './components/MainDash/MainDash';
+import RightSide from './components/RigtSide/RightSide';
+import Sidebar from './components/Sidebar';
+
 function App() {
   return (
-    <>
-       <Authentication>
-        <Routes>
-             
-            <Route path='/' element={<Home/>}>
-
-
-            </Route>
-            <Route path='/dashboard' element={<Dashboard/>}>
-
-
-            </Route>
-          
-           <Route path='/admin' element={<Admin/>}>
-                    <Route index element={<AdminLoginRequired><AdminPanel/></AdminLoginRequired>}></Route>
-                    <Route path='login' element={<AdminLogin/>}/>
-
-
-
-           </Route>
-
-        </Routes>
-       </Authentication>
-
-
-
-    </>
+    <div className="App">
+      <div className="AppGlass">
+        <Sidebar/>
+        {/* <MainDash/> */}
+        {/* <RightSide/> */}
+      </div>
+    </div>
   );
 }
+
 export default App;
