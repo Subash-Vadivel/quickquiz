d4a17b4d5a32909b88e99cd9239a53355e24c74b
Subash-Vadivel <88760720+Subash-Vadivel@users.noreply.github.com>
2023-03-09 14:19:52 +0530

    ASA

--- src/Resources/User_components/Question/AutoLayoutExample.js
+++ src/Resources/User_components/Question/AutoLayoutExample.js
@@ -1,76 +1,101 @@
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 // import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
 import style from './sb.module.css'
 import { Form,Button, Container } from 'react-bootstrap';
 import Sidebar from './Sidebar';
 import Timer from './Timer';
-import { useEffect } from 'react';
+import { useEffect,useState } from 'react';
 import axiosPrivate from '../../../Api/axiosPrivate';
 import { useParams } from 'react-router-dom';
 function AutoLayoutExample() {
-  const id=useParams()
+  const param=useParams()
+  const [question,setQuestion]=useState([]);
+  const [basic,setBasic]=useState([]);
+  const [currentQ,setCurrentQ]=useState(1);
+  const [status,setStatus]=useState(false);
+  const [answer,setAnswer]=useState('');
   const load=async()=>{
-    await axiosPrivate.get()
+    await axiosPrivate.get(`question/${param.id}`).then((res)=>{setQuestion(res.data.data.question.questions);setBasic(res.data.data.question);console.log(res.data.data.question);setStatus(true)}).catch((err)=>{console.log(err)});
   }
   useEffect(()=>{
-
+     load();
   },[])
 
   return (
-    <Container fluid className={style.margina}>  
-    <Container fluid className={`shadow ${style.header}`}>
-    <Row>
-      <Col md={10}>
-        <h2>&nbsp;Aptitude</h2>
-      </Col>
-      <Col md={1}>
-        <Timer />
-      </Col>
-      <Col md={1}>
-        <Button variant="success">Submit</Button>{' '}
-        <br/>
-        <br/>
-      </Col>
-    </Row>
-  </Container>
-    <br/>
-    
-    <Container fluid>
-    <Row>
-    <Col md={4} >
-    <div>
-       <h5>Question 1</h5>
-      <h6 style={{ display: 'inline-block', marginRight: '10px'}}>Multi Choice Type Question</h6>
-      <h6 style={{ display: 'inline-block',float:'right' }}>Marks:1</h6> 
-      <p>The last day of century cannot be:</p>
-    </div>
-  </Col>
-      <Col md={5} >
-      <br/>
-        <Form.Control type="text" placeholder="Monday" onMouseOver={(e) => e.target.style.cursor = "pointer"} readOnly /> <br/>
-        <Form.Control type="text" placeholder="Wednesday" onMouseOver={(e) => e.target.style.cursor = "pointer"}readOnly /> <br/>
-        <Form.Control type="text" placeholder="Friday" onMouseOver={(e) => e.target.style.cursor = "pointer"}readOnly /> <br/>
-        <Form.Control type="text" placeholder="Tuesday" onMouseOver={(e) => e.target.style.cursor = "pointer"} readOnly /> <br/>
-        <Button variant="danger">Clear</Button>{' '}
-        <br/>
-        <br/>
-      </Col>
-      <Col md={3} className="text-end">
-       <Sidebar/>
-      </Col>
-    </Row>
-      <br/>
-      <footer style={{textAlign:"center"}}>
-      <Button variant="primary" size="md">
-        back
-      </Button>{' '}
-      <Button variant="primary" size="md">
-      next
-    </Button>{' '}
-    </footer>
-      </Container>
+   <>
+   {
+    status ?
+
+    (
+      <Container fluid className={style.margina}>  
+      <Container fluid className={`shadow ${style.header}`}>
+      <Row>
+        <Col md={10}>
+          <h2>&nbsp;Aptitude</h2>
+        </Col>
+        <Col md={1}>
+          <Timer />
+        </Col>
+        <Col md={1}>
+          <Button variant="success">Submit</Button>{' '}
+          <br/>
+          <br/>
+        </Col>
+      </Row>
     </Container>
+      <br/>
+      
+      <Container fluid>
+      <Row>
+      <Col md={4} >
+      <div>
+         <h5>Question {currentQ}</h5>
+        <h6 style={{ display: 'inline-block', marginRight: '10px'}}>Multi Choice Type Question</h6>
+        <h6 style={{ display: 'inline-block',float:'right' }}>Marks: {question[currentQ-1].mark}</h6> 
+        <p>{question[currentQ-1].question}</p>
+      </div>
+    </Col>
+        <Col md={5} >
+        <br/>
+  
+            {
+              question[currentQ-1].choices.map((data,index)=>
+               <Form.Control  key={index} type="text" value={data} onClick={(e) => setAnswer(e.target.value)} className={style.choiceForm} readOnly />
+              )
+            }
+          <Button variant="danger" onClick={()=>{setAnswer('')}}>Clear</Button>{' '}
+          <br/>
+          <br/>
+        </Col>
+        <Col md={3} className="text-end">
+         <Sidebar/>
+        </Col>
+      </Row>
+        <br/>
+        <footer style={{textAlign:"center"}}>
+        <Button variant="primary" size="md">
+          back
+        </Button>{' '}
+        <Button variant="primary" size="md">
+        next
+      </Button>{' '}
+      </footer>
+        </Container>
+      </Container> 
+    ):
+
+    ( 
+      <h1>Some thing went Wrong Try again</h1>
+
+
+    )
+
+   }
+   
+   
+   </>
   );
 }
 
