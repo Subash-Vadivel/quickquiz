23ef8f256cb7c4f122f91d90f8298f402f52f3b0
LalitKumaran <lalitkumaran1602@gmail.com>
2023-03-10 14:59:01 +0530

    filter set

--- src/Resources/User_Pages/Practice.js
+++ src/Resources/User_Pages/Practice.js
@@ -7,7 +7,6 @@ import { Card, Row, Col, Container, Form, Button} from 'react-bootstrap';
 import {
     CDBSidebar,
     CDBSidebarContent,
-    CDBSidebarSubMenu,
     CDBSidebarHeader,
     CDBSidebarMenu,
     CDBSidebarMenuItem,
@@ -17,25 +16,128 @@ import axiosPrivate from '../../Api/axiosPrivate';
 
 
 export default function Practice() {
+    
 const navigate = useNavigate()
+
 const auth=useAuth();
+
 const [practice,setPractice] = useState([])
+
 const [filter,setFilter] = useState([])
 
+const [category,setCategory] = useState([])
+
+const [checked,setChecked] = useState({'Easy': false,'medium': false, 'Hard': false})
+
+const [currentFilter,setCurrentFilter] = useState({mode:[],categoryName:[]})
+
+const [state,setState] = useState(0)
+
 const load=async()=>{
-    
         await axiosPrivate.get('/question').then((res)=>{
-            setPractice(res.data.data.allQuestions); setFilter(res.data.data.allQuestions);console.log(res.data.data.allQuestions)
+            setPractice(res.data.data.allQuestions); 
+            setFilter(res.data.data.allQuestions);
+            var questionList = res.data.data.allQuestions
+            var checkedList = checked
+            var categoryList = []
+            questionList.map((data) => {
+                if(categoryList.includes(data.categoryName) === false){
+                    categoryList.push(data.categoryName)
+                    checkedList[data.categoryName] = false
+                }
+            })
+            setCategory(categoryList)
+            setChecked({...checkedList})
         }).catch((err)=>{console.log(err)})
+        
     //   setPractice([{"title":"Advanced Python Programming","body":""},{"title":"Java Object Oriented Programming","body":""},
     // {"title":"Introduction to Modern C++","body":""},{"title":"C Language","body":""}])
-      // axios.get('https://jsonplaceholder.typicode.com/posts').then((res)=>{ setLearn(res.data)})
-    
+    // axios.get('https://jsonplaceholder.typicode.com/posts').then((res)=>{ setLearn(res.data)})
+}
+
+
+const isChecked = (e) => {
+    var checkedList = checked
+    checkedList[e.target.id] = !checkedList[e.target.id]
+    setChecked({...checkedList})
+}
+
+const updateFilter = () => {
+    var changeFilter = []
+    if(currentFilter.categoryName.length === 0 && currentFilter.mode.length !== 0 ){
+        practice.map((data,index)=>{
+            if(currentFilter.mode.includes(data.mode) === true){
+                changeFilter.push(data)
+            }
+        })
+    }
+    else if(currentFilter.mode.length === 0 && currentFilter.categoryName.length !== 0){
+        practice.map((data,index)=>{
+            if(currentFilter.categoryName.includes(data.categoryName) === true){
+                changeFilter.push(data)
+            }
+        })
+    }
+    else if(currentFilter.mode.length !== 0 && currentFilter.categoryName.length !== 0){
+        practice.map((data,index)=>{
+            if(currentFilter.mode.includes(data.mode) === true && currentFilter.categoryName.includes(data.categoryName) === true ){
+                changeFilter.push(data)
+            }
+        })
+    }
+    setFilter(changeFilter)
+    setState(0);
+}
+
+const clearFilter = () => {
+    var checkedList = checked
+    Object.keys(checkedList).map(data=>{
+        if(checkedList[data] === true){
+            checkedList[data] = false
+        }
+    })
+    setCurrentFilter({mode: [], categoryName: []})
+    setChecked(checkedList)
+    setFilter([...practice])
+}
+
+const applyFilter = () => {
+    var checkedList = checked
+    var currFilters = {mode: [], categoryName: []}
+    Object.keys(checkedList).map(data=>{
+        if(checkedList[data] === true){
+            if(['Easy','medium','Hard'].includes(data) === true){
+                if(currFilters.mode.length === 0){
+                    currFilters.mode.push(data)
+                }
+                else if(currFilters.mode.includes(data)===false){
+                    currFilters.mode.push(data)
+                }
+            }
+            else{
+                if(currFilters.categoryName.length === 0){
+                    currFilters.categoryName.push(data)
+                }
+                else if(currFilters.categoryName.includes(data)===false){
+                    currFilters.categoryName.push(data)
+                }
+            }
+            
+        }
+    })
+    setCurrentFilter(currFilters)
+    setState(1)
+
 }
 
 useEffect(()=>{
     load();
+    
 },[])
+useEffect(()=>{
+    updateFilter()
+},[state])
+
 
 return (
     <>
@@ -47,12 +149,9 @@ return (
     <br></br>    
 
 
-
-
     <div className={practice_styles.dFlex}>
-      <div className={practice_styles.fixedSideBar}>
+    <div className={practice_styles.fixedSideBar}>
     
-
 
     <CDBSidebar className={practice_styles.filterbox}>
                 <CDBSidebarHeader className={practice_styles.filterbox_header} prefix={<i className="fa fa-filter fa-small"></i>}>
@@ -62,44 +161,57 @@ return (
                 </CDBSidebarHeader>
                 <CDBSidebarContent className={practice_styles.filterbox_content}>
                     <CDBSidebarMenu className={practice_styles.filterbox_menu}>
+                        <CDBSidebarMenuItem className={practice_styles.filterbox_alter}>
+                            <Row>
+                            <Col><Button variant='danger' onClick={clearFilter}>Clear</Button></Col>
+                            <Col><Button variant='success'onClick={applyFilter}>Apply</Button></Col>
+                            </Row>
+                        </CDBSidebarMenuItem>
                         <CDBSidebarMenuItem>
                            Difficulty
                         </CDBSidebarMenuItem>
                         <CDBSidebarMenuItem><Form>
                             <Form.Check 
                                 type={"Checkbox"}
-                                id={`default`}
+                                id={`Easy`}
+                                checked={checked['Easy']}
+                                onChange={isChecked}
                                 label={`Easy`}
                             /></Form>
                         </CDBSidebarMenuItem>
                         <CDBSidebarMenuItem><Form>
                             <Form.Check 
                                 type={"Checkbox"}
-                                id={`default`}
+                                id={`medium`}
+                                checked={checked['medium']}
+                                onChange={isChecked}
                                 label={`Medium`}
                             /></Form>
                         </CDBSidebarMenuItem>
                         <CDBSidebarMenuItem><Form>
                             <Form.Check 
                                 type={"Checkbox"}
-                                id={`default`}
+                                id={`Hard`}
+                                checked={checked['Hard']}
+                                onChange={isChecked}
                                 label={`Hard`}
                             /></Form>
                         </CDBSidebarMenuItem>
                         <CDBSidebarMenuItem>
-                            Topics
+                            Category
                         </CDBSidebarMenuItem>
-                        <CDBSidebarMenuItem>
+                        {category.map((data,index) =>
+                        <CDBSidebarMenuItem key={index}>
                         <Form>
                             <Form.Check 
                                 type={"Checkbox"}
-                                id={`default`}
-                                label={`Title`}
+                                id={`${data}`}
+                                checked={checked[`${data}`]}
+                                onChange={isChecked}
+                                label={`${data}`}
                             /></Form>
                         </CDBSidebarMenuItem>
-                        <CDBSidebarMenuItem className={practice_styles.filterbox_footer}>
-                            <Button variant='success'>Apply</Button>
-                        </CDBSidebarMenuItem>
+                        )}
                     </CDBSidebarMenu>
                 </CDBSidebarContent>
                 
@@ -116,13 +228,21 @@ return (
               { filter.map((data,index)=> 
             <Col key={index}>
                 <Card className={practice_styles.questionpaper} >
-                    <Card.Title>Topic : {data.topic}</Card.Title>
+                    <Card.Title>
+                        <Row><Col sm={9}>Topic : {data.topic}</Col><Col>Time : </Col></Row></Card.Title>
                     <Card.Body>
-                    <Card.Text>Category : {data.categoryName}</Card.Text>
-                    <Card.Text>Total Questions : {data.questions.length}</Card.Text>
-                    <Card.Text>Difficulty Level : {data.mode}</Card.Text>
-                    <Card.Text>Time :</Card.Text>
-                    <Button onClick={()=>navigate('/id')} variant='success'>Practice</Button>
+                    <Row>
+                    <Col sm={6}><Card.Text>Category :  {data.categoryName}</Card.Text></Col>
+                    <Col sm={6}><Card.Text>Difficulty Level :  {data.mode}</Card.Text></Col>
+                    </Row>
+                    <Row>
+                    <Col sm={6}><Card.Text>Total Questions : {data.questions.length}</Card.Text></Col>
+                    <Col sm={6}><Card.Text></Card.Text></Col>
+                    </Row>
+                    <Row>
+                    <Col sm = {10}></Col>
+                    <Col><Button onClick={()=>navigate('/id')} variant='success'>Practice</Button></Col>
+                    </Row>
                     </Card.Body>
                 </Card>
             </Col>
