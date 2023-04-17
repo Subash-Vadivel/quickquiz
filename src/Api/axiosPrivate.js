f974279b9d97603dad7fe8f8fd7bc8fe08e76734
LalitKumaran <lalitkumaran1602@gmail.com>
2023-04-17 13:45:55 +0530

    points

--- src/Api/axiosPrivate.js
+++ src/Api/axiosPrivate.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 // https://quick-quiz.onrender.com
-const BASE_URL="http://localhost:5500";
+// const BASE_URL="http://localhost:5500";
+const BASE_URL="https://quick-quiz.onrender.com";
 export default axios.create({
     baseURL:BASE_URL
 
